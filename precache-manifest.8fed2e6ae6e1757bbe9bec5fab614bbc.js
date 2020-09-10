d8fc8bcef0fd5bcdb7634d55d4a27c83c1e6fb70
sobchuksasha <sobchuksasha1995@gmail.com>
2020-09-10 23:21:03 +0300

    Updates

---
+++ precache-manifest.8fed2e6ae6e1757bbe9bec5fab614bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2feaf0bcd6c0cd7b2d6714870f5c3cf1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aef9f96a3a54a66b5cdf",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "1a5ab6f61ec13e4308e4",
+    "url": "./static/js/2.6da940cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6da940cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef9f96a3a54a66b5cdf",
+    "url": "./static/js/main.c178dbb0.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
